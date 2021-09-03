ac0d8aaf799c51a2753cc190c14ecfc5e7a2ab02
AlyaVil <89807857+AlyaVil@users.noreply.github.com>
2021-09-03 11:35:28 +0300

    win

---
+++ Human.java
@@ -0,0 +1,17 @@
+
+public class Human {
+    int age = 22;
+
+    public Human(int age_arg) {
+        age = age_arg;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age_arg) {
+        age = age_arg;
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+
+public static void main(String[] args) {
+    int humanAge=22;
+    Human human= new Human(humanAge);
+    System.out.println ("Main: main(): human.getAge()=" +human.getAge());
+}}
